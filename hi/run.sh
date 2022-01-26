4535180710f0dbd0679e51ad2c1595fc5e577439
Anurag Singh <anurag.comm@gmail.com>
2022-01-26 18:00:56 +0530

    fix hi run.sh

--- hi/run.sh
+++ hi/run.sh
@@ -5,11 +5,10 @@ python run_speech_recognition_ctc.py \
 --output_dir="./" \
 --overwrite_output_dir \
 --num_train_epochs="100" \
---per_device_train_batch_size="32" \
+--per_device_train_batch_size="8" \
 --per_device_eval_batch_size="16" \
---gradient_accumulation_steps="1" \
+--gradient_accumulation_steps="4" \
 --learning_rate="7.5e-5" \
---lr_scheduler_type="linear" \
 --warmup_steps="2000" \
 --length_column_name="input_length" \
 --evaluation_strategy="steps" \
